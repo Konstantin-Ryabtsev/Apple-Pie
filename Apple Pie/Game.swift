17824888b1f5c26a0a8c669b9022d4101c161cdc
Konstantin Ryabtsev <kryabtsev83@gmail.com>
2021-12-22 00:11:53 +0300

    Added Game model

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Konstantin Ryabtsev on 21.12.2021.
+//
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -2,18 +2,77 @@
 //  ViewController.swift
 //  Apple Pie
 //
-//  Created by Konstantin on 21.12.2021.
+//  Created by Konstantin Ryabtsev on 21.12.2021.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IB Outlets
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet var letterButtons: [UIButton]!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    // MARK: - Properties
+    var currentGame: Game!
+    let incorrectMovesAllowed = 7
+    var listOfWords = [
+        "Барсук",
+        "Бабочка",
+        "Белка",
+        "Бурундук",
+        "Медведь",
+        "Волк",
+        "Енот",
+        "Ёж",
+        "Жук",
+        "Заяц",
+        "Зубр",
+        "Кабан",
+        "Крот",
+        "Куница",
+        "Лисица",
+        "Лось",
+        "Мангуст",
+        "Муравьи",
+        "Мышь",
+        "Олень",
+        "Опоссум",
+        "Росомаха",
+        "Рысь",
+        "Скунс",
+        "Соболь",
+        "Стрекоза",
+        "Тигр"
+    ]
+    var totalWins = 0
+    var totalLosses = 0
+
+    // MARK: - Methods
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed)
+        updateUI()
+    }
+    
+    func updateUI() {
+        let movesRemaining = currentGame.incorrectMovesRemaining
+        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        treeImageView.image = UIImage(named: image)
+        scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        newRound()
     }
-
-
+    
+    //MARK: - IB Actions
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
