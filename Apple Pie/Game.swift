a83e4ff6e630e1ebb49e7a92adca20332c1b6d57
Konstantin Ryabtsev <kryabtsev83@gmail.com>
2021-12-22 14:53:00 +0300

    Implemented correct word label update

--- Apple Pie/Game.swift
+++ Apple Pie/Game.swift
@@ -8,4 +8,32 @@
 struct Game {
     var word: String
     var incorrectMovesRemaining: Int
+    private var guessedLetters = [Character]()
+    
+    init(word: String, incorrectMovesRemaining: Int) {
+        self.word = word
+        self.incorrectMovesRemaining = incorrectMovesRemaining
+    }
+    
+    var guessedWord: String {
+        var wordToShow = String()
+        
+        for letter in word {
+            if guessedLetters.contains(Character(letter.lowercased())) {
+                wordToShow += String(letter)
+            } else {
+                wordToShow += "_"
+            }
+        }
+        
+        return wordToShow
+    }
+    
+    mutating func playerGuessed(letter: Character) {
+        let lowercasedLetter = Character(letter.lowercased())
+        guessedLetters.append(lowercasedLetter)
+        if !word.lowercased().contains(lowercasedLetter) {
+            incorrectMovesRemaining = incorrectMovesRemaining <= 0 ? 0 : incorrectMovesRemaining - 1
+        }
+    }
 }
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -57,10 +57,19 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    func updateCorrectWordLabel() {
+        var displayWord = [String]()
+        for letter in currentGame.guessedWord {
+            displayWord.append(String(letter))
+        }
+        correctWordLabel.text = displayWord.joined(separator: " ")
+    }
+    
     func updateUI() {
-        let movesRemaining = currentGame.incorrectMovesRemaining
-        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        let movesRemaining = (currentGame.incorrectMovesRemaining + 8) % 8
+        let image = "Tree\(movesRemaining)"
         treeImageView.image = UIImage(named: image)
+        updateCorrectWordLabel()
         scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
     }
     
@@ -72,6 +81,9 @@ class ViewController: UIViewController {
     //MARK: - IB Actions
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letter = sender.title(for: .normal)!
+        currentGame.playerGuessed(letter: Character(letter))
+        updateUI()
     }
     
 }
